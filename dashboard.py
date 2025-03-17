c7a2f789459009d7a9b503c89539236e94af297b
Jibril Nikki <114635288+nikkids@users.noreply.github.com>
2025-03-17 15:25:47 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-day_df = pd.read_csv("dashboard/day.csv")
-hour_df = pd.read_csv("dashboard/hour.csv")
+day_df = pd.read_csv("day.csv")
+hour_df = pd.read_csv("hour.csv")
 
 
 st.title("Bikesharing")
