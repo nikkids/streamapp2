f9c6601dab69e22bb46794584a80ded3576c86a6
Jibril Nikki <114635288+nikkids@users.noreply.github.com>
2025-03-19 22:10:14 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,60 +3,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-day_df = pd.read_csv("day.csv")
-hour_df = pd.read_csv("hour.csv")
+day_df = pd.read_csv("dashboard/day.csv")
 
 
-st.title("Bikesharing")
-
-
-st.header("Dataset Overview")
-st.write("Sample data harian")
-st.dataframe(day_df.head())
-st.write("Sample data per jam")
-st.dataframe(hour_df.head())
-
-
-st.subheader("Missing Value")
-st.write(day_df.isna().sum())
-st.write(hour_df.isna().sum())
-st.write("Tidak ada missing value")
-
-
-
-st.header("Tren User Seiring Waktu")
-st.write("Per Tahun")
 day_df['dteday'] = pd.to_datetime(day_df['dteday'])
-selected_year = st.slider("Pilih Tahun", 2011, 2012)
-filtered_data = day_df[day_df['dteday'].dt.year == selected_year]
+
+
+st.title("Dashboard Bike Sharing")
+
+
+st.sidebar.header("Filter")
+selected_year = st.sidebar.selectbox("Pilih Tahun", day_df['dteday'].dt.year.unique())
+
+
+data_filtered = day_df[day_df['dteday'].dt.year == selected_year]
+
+st.subheader("PERTANYAAN BISNIS 1")
 fig, ax = plt.subplots(figsize=(10, 5))
-sns.lineplot(x='dteday', y='cnt', data=filtered_data, ax=ax)
-ax.set_ylabel("Total User per Hari")
-st.pyplot(fig)
-
-st.header("Tren User Seiring Waktu")
-st.write("Per Bulan")
-day_df['dteday'] = pd.to_datetime(day_df['dteday'])
-selected_month = st.selectbox("Pilih Bulan", list(range(1, 13)))
-filtered_data = day_df[(day_df['dteday'].dt.year == selected_year) & (day_df['dteday'].dt.month == selected_month)]
-fig, ax = plt.subplots(figsize=(10, 5))
-sns.lineplot(x='dteday', y='cnt', data=filtered_data, ax=ax)
-ax.set_ylabel("Total user per Hari")
+sns.barplot(data=data_filtered, x=data_filtered['dteday'].dt.strftime('%B'), y='registered', color='blue', label='Registered', errorbar=None)
+sns.barplot(data=data_filtered, x=data_filtered['dteday'].dt.strftime('%B'), y='casual', color='orange', label='Casual', errorbar=None)
+plt.xticks(rotation=45)
+plt.xlabel("Bulan")
+plt.ylabel("Total User")
+plt.legend()
 st.pyplot(fig)
 
 
-st.header("User Casual vs User Registered")
-fig, ax = plt.subplots()
-day_df[['casual', 'registered']].sum().plot(kind='bar', ax=ax, color=['blue', 'green'])
-ax.set_ylabel("Total User")
-st.pyplot(fig)
+Periode = data_filtered.copy()
+Periode.set_index('dteday', inplace=True)
+Periode_monthly = Periode.resample('M').mean().reset_index()
 
-
-
-
-st.header("Fitur Korelasi Heatmap")
-if st.checkbox("Tampilkan Correlation Matrix"):
-    fig, ax = plt.subplots(figsize=(10, 5))
-    sns.heatmap(day_df.corr(), cmap='coolwarm', ax=ax)
-    st.pyplot(fig)
+st.subheader("PERTANYAAN BISNIS 2")
+fig2, ax2 = plt.subplots(figsize=(12, 6))
+sns.scatterplot(x='dteday', y='registered', data=Periode_monthly, s=100, color='blue')
+sns.lineplot(x='dteday', y='registered', data=Periode_monthly, linestyle='-')
+plt.title('Rata-Rata Registrasi Tiap Bulan', fontsize=14)
+plt.xlabel('Bulan', fontsize=12)
+plt.ylabel('Jumlah Registrasi Rata-Rata', fontsize=12)
+plt.xticks(rotation=45)
+plt.grid(True)
+st.pyplot(fig2)
